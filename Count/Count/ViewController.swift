2940411bd72f59d416a8e6ce7946fcb1bcd9ae95
Tomoya113 <tomoya113.code@gmail.com>
2020-09-02 13:21:10 +0900

    done Count

---
+++ Count/Count/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by tomoya tanaka on 2020/09/02.
+//  Copyright © 2020 Tomoya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func minus() {
+        number -= 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func multiply() {
+        number = number * 2
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func divide() {
+        number = number / 2
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
+    }
+}
+
