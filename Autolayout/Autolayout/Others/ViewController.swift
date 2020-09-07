c7e7318dc1403d788bc39547d7c1ed8716cb28ee
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-09-07 21:01:54 +0900

    done Autolayout

---
+++ Autolayout/Autolayout/Others/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by 張翔 on 2019/06/30.
+//  Copyright © 2019 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
