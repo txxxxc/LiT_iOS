03ab24e83b4b3a9b62092b15f137c4cd8f8c7db6
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-09-21 04:09:58 +0900

    done profile

---
+++ Profile/Profile/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by tomoya tanaka on 2020/09/21.
+//  Copyright © 2020 Tomoya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileLabel: UILabel!
+    
+    @IBOutlet var profileCommentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func tapButton1() {
+        profileImageView.image = UIImage(named: "philImage")
+        profileLabel.text = "名前"
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+    }
+    
+    @IBAction func tapButton2() {
+        profileImageView.image = UIImage(named: "trackImage")
+        profileLabel.text = "スポーツ"
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+    }
+    
+    @IBAction func tapButton3() {
+        profileImageView.image = UIImage(named: "appleImage")
+        profileLabel.text = "好きな食べ物"
+        profileCommentLabel.text = "りんごが好きで、いつも持ち歩いている"
+    }
+    
+    @IBAction func tapButton4() {
+        profileImageView.image = UIImage(named: "flightImage")
+        profileLabel.text = "趣味"
+        profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+    }
+}
+
